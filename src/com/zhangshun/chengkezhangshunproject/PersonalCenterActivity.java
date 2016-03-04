c6dd6baf0a99a6d6a4c5226fb354f0b4a7691fef
RuffianZhang <tangmenshun@foxmail.com>
2016-03-04 10:03:37 +0800

    add btn in PresonActiviity

--- src/com/zhangshun/chengkezhangshunproject/PersonalCenterActivity.java
+++ src/com/zhangshun/chengkezhangshunproject/PersonalCenterActivity.java
@@ -2,15 +2,16 @@ package com.zhangshun.chengkezhangshunproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class PersonalCenterActivity extends Activity {
-
+	Button btn;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.personal_center);
-
+		
 	}
 
 }
