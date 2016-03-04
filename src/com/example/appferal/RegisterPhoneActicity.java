32e2795cf9c52416ba1012b74277038d81bf1bd9
ruanjiawei <297216624@qq.com>
2016-03-04 17:16:06 +0800

    6666666666666666666666666666

---
+++ src/com/example/appferal/RegisterPhoneActicity.java
@@ -0,0 +1,12 @@
+package com.example.appferal;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RegisterPhoneActicity extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.register_phone);
+	}
+}
