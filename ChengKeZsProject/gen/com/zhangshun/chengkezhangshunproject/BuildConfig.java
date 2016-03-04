bdf559409bf4391c0692865dbaba8f14a6034714
RuffianZhang <tangmenshun@foxmail.com>
2016-03-04 09:58:23 +0800

    frist commit

--- ChengKeZsProject/gen/com/zhangshun/chengkezhangshunproject/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.zhangshun.chengkezhangshunproject;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
