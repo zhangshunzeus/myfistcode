ee1f22e9a22916f18d2cd66a08d9932d4d9e3b64
ruanjiawei <297216624@qq.com>
2016-03-07 10:42:03 +0800

    页面跳转

--- src/com/example/appferal/LoginActivity.java
+++ src/com/example/appferal/LoginActivity.java
@@ -1,13 +1,59 @@
 package com.example.appferal;
 
+import com.zhangshun.chengkezhangshunproject.HomePageActivity;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class LoginActivity extends Activity {
- @Override
-protected void onCreate(Bundle savedInstanceState) {
-	// TODO Auto-generated method stub
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.login);
-}
+	Button btn,register_btn,password_btn;
+	ImageView back;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login);
+		btn = (Button) findViewById(R.id.login_butn);
+		btn.setOnClickListener(l);
+		register_btn=(Button) findViewById(R.id.register_btn);
+		register_btn.setOnClickListener(l);
+		password_btn=(Button) findViewById(R.id.password_btn);
+		password_btn.setOnClickListener(l);
+		back=(ImageView) findViewById(R.id.fanhui);
+		back.setOnClickListener(l);
+	}
+
+	OnClickListener l = new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			switch (v.getId()) {
+			case R.id.login_butn:
+				Intent intent = new Intent(LoginActivity.this, HomePageActivity.class);
+				startActivity(intent);
+				break;
+			case R.id.register_btn:
+				Intent inten = new Intent(LoginActivity.this, RegisterPhoneActicity.class);
+				startActivity(inten);
+				break;
+			case R.id.password_btn:
+				Intent intents = new Intent(LoginActivity.this, SetPasswordActivity.class);
+				startActivity(intents);
+				break;
+			case R.id.fanhui:
+				Intent fanhui = new Intent(LoginActivity.this, Login_registerActivity.class);
+				startActivity(fanhui);
+				break;
+			default:
+				break;
+			}
+		}
+	};
 }
--- src/com/example/appferal/RegisterPhoneActicity.java
+++ src/com/example/appferal/RegisterPhoneActicity.java
@@ -1,12 +1,35 @@
 package com.example.appferal;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class RegisterPhoneActicity extends Activity {
+	Button next;
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.register_phone);
+		next=(Button) findViewById(R.id.next);
+		next.setOnClickListener(l);
 	}
+	OnClickListener l=new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			switch (v.getId()) {
+			case R.id.next:
+				Intent intent = new Intent(RegisterPhoneActicity.this, RegisterInfoActivity.class);
+				startActivity(intent);
+				break;
+
+			default:
+				break;
+			}
+		}
+	};
 }
