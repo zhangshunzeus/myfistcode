ed5ddecf5b79712330ba2194ec727b3dc8f3f481
unknown <>
2016-03-07 10:30:42 +0800

    张荣提交代码

--- src/com/zhangshun/activity/MainActivity.java
+++ src/com/zhangshun/activity/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_zhangshunmain);
 		for (int i = 0; i < bdi.length; i++) {
 			butto = (Button) findViewById(bdi[i]);
 			butto.setOnClickListener(listener);
